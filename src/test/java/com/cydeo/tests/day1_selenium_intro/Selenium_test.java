34c5938a10ed162266426ba78718367d2c645ceb
Adem OZTURK <admoztrk86@gmail.com>
2022-12-19 01:09:35 +0100

    New changes added

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Selenium_test.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Selenium_test.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selenium_test {
     public static void main(String[] args) {
         //1- Setting up the web driver manager
+        // We create our " browser driver"
         WebDriverManager.chromedriver().setup();
 
         // 2- Create instance of the chrome driver
+        // This command will open our "browser"
         WebDriver driver = new ChromeDriver();
 
         // 3- Test if the driver is working fine
+        // This is our selenium method allowing us to get the target
         driver.get("https://www.youtube.com");
 
 
